b2884504532c1cf0d88778dafbcbc611e3b043d2
Hamjaster <hamzasepal@gmail.com>
2023-11-24 11:42:27 +0500

    firebase fixed

--- carnatic-frontend/firebase.config.js
+++ carnatic-frontend/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD8HzOtuPfBz1emqYIVSqivWShNlgSyT6o",
-    authDomain: "next-project-20a19.firebaseapp.com",
-    projectId: "next-project-20a19",
-    storageBucket: "next-project-20a19.appspot.com",
-    messagingSenderId: "429167109043",
-    appId: "1:429167109043:web:19c4c9018ded44d689a1c6"
+    apiKey: "AIzaSyBlAee8pAF6yzOcmR6V8sPNcmftbRYATxg",
+    authDomain: "daraz-clone-1beab.firebaseapp.com",
+    projectId: "daraz-clone-1beab",
+    storageBucket: "daraz-clone-1beab.appspot.com",
+    messagingSenderId: "331120664694",
+    appId: "1:331120664694:web:120374edcacd29e0de7f57"
 };
 
 // Initialize Firebase
