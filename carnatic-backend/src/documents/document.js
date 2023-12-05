b8724c7f3d91b7972e01e0df5d7e23543d86b5b5
Hamjaster <hamzasepal@gmail.com>
2023-12-05 16:54:08 +0500

    backend changes

--- carnatic-backend/src/documents/document.js
+++ carnatic-backend/src/documents/document.js
@@ -23,141 +23,165 @@ const giveHTML = ({ name, email, phone, PAN, address, amount, project }) => {
     </head>
     
     <body style="font-family: Arial, sans-serif;margin: 0; padding: 16px;">
-    <div style = "font-size : 19px">
-    Dear <b>${name} </b>, <br>
-We sincerely thank you for your generous donation of <b>INR ${amount}</b> in support of <b>${project}</b>. It is through the incredible support of donors like you that we are able to continue providing crucial projects. We truly appreciate you making the choice not only to contribute to Carnatic Foundation today but also to stand with us as a supporter in the future.  <br>
-Please find the receipt below.   <br>
-Thank you once again for your tremendous generosity. We are so grateful for your support.  <br>
-Yours truly,  <br>
-Carnatic Foundation <br>
-P.S : Tax Exemption certificate will be sent to you shortly.  <br>
 
+    
+    <div style="font-size : 15px; line-height: 25px">
+    Dear <b>${name} </b>,
+    <div style="margin-top: 5px;">
+        &emsp; &emsp; &emsp;We sincerely thank you for your generous donation of <b>INR ${amount}</b> in support of
+        <b>${project}</b>.
+        It is through the incredible support of donors like you that we are able to continue providing crucial
+        projects. We truly appreciate you making the choice not only to contribute to Carnatic Foundation today but
+        also to stand with us as a supporter in the future.
     </div>
+    <div style="margin-bottom: 24px; margin-top: 5px;">
+        Please find the receipt below. Thank you once again for your tremendous generosity. We are so grateful for
+        your support.
+    </div>
+
+    <div> Yours truly,</div>
+    <div> Carnatic Foundation</div>
+    <div style="margin-top: 22px;"> P.S : Tax Exemption certificate will be sent to you shortly.</div>
+
+</div>
+
     
-        <table width="100%" border="0" cellpadding="0" cellspacing="0"
-            style="border: 2px solid black; width: 30rem; font-size: 0.875rem; padding : 20px; margin : 44px 0px;">
-            <!-- Header -->
+<table width="100%" border="0" cellpadding="0" cellspacing="0"
+style="border: 2px solid black; width: 30rem; font-size: 0.875rem; padding : 20px; margin : 44px 0px;">
+<!-- Header -->
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <table width="100%" cellpadding="0" cellspacing="0">
             <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <table width="100%" cellpadding="0" cellspacing="0">
-                        <tr>
-                            <td style="border-bottom: 2px solid black; text-align: left;">
-                                <img style="width: 7rem;" src="https://i.postimg.cc/vmH5jVzy/logo.jpg" alt="Logo" />
-                            </td>
-                            <td style="border-bottom: 2px solid black; text-align: center; padding: 1rem;">
-                                <b style="font-weight: bold;">CARNATIC FOUNDATION</b><br>
-                                <span>No:2, DEJU PLAZA, SOUTH AVENUE, SRINAGAR COLONY, SAIDAPET, CHENNAI - 600029</span>
-                            </td>
-                        </tr>
-                    </table>
+                <td style="border-bottom: 2px solid black; text-align: left;">
+                    <img style="width: 7rem;" src="https://i.postimg.cc/vmH5jVzy/logo.jpg" alt="Logo" />
+                </td>
+                <td style="border-bottom: 2px solid black; text-align: center; padding: 1rem;">
+                    <b style="font-weight: bold;">CARNATIC FOUNDATION</b><br>
+                    <span>No:2, DEJU PLAZA, SOUTH AVENUE, SRINAGAR COLONY, SAIDAPET </span>
+                    <div>CHENNAI - 600029</div>
                 </td>
             </tr>
-    
-            <!-- Receipt details -->
+        </table>
+    </td>
+</tr>
+
+<!-- Receipt details -->
+<tr>
+    <td colspan="3" style="border-bottom: 2px solid black; padding : 8px 0px">
+        <table width="100%" cellpadding="0" cellspacing="0">
+            <tbody>
+                <tr
+                    style="/* display: flex; *//* justify-content: space-between; *//* align-items: center; */font-size: 14px;">
+                    <td style="
+width: 33%;
+">No. 123</td>
+                    <td style="font-weight: bold;width: 33%;/* font-size: 13px; */">DONATION RECEIPT</td>
+                    <td style="
+width: 33%;
+">Date : Mon 22 Nov</td>
+                </tr>
+            </tbody>
+        </table>
+    </td>
+</tr>
+
+<!-- Other sections... -->
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <div style="padding: 0.5rem 0; text-align: center;"><b>Donations are exempted Under 80-G of the Income
+                Tax Act 1961</b></div>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
             <tr>
-                <td colspan="3" style="border-bottom: 2px solid black; padding : 8px 0px">
-                    <table width="100%" cellpadding="0" cellspacing="0">
-                        <tr>
-                            <td>No. ${uniqueNumber}</td>
-                            <td style="font-weight: bold;">DONATION RECEIPT</td>
-                            <td>Date. ${currentDate.toDateString()}</td>
-                        </tr>
-                    </table>
+                <td><b>Form 12 A No:</b> AABTC8873EF2021101</td>
+                <td><b>REG NO:</b> 56/2016</td>
+            </tr>
+            <tr>
+                <td><b>80 G NUMBER:</b> AABTC8873EF20211</td>
+                <td><b>PAN NO:</b> AABTC8873E</td>
+            </tr>
+        </table>
+    </td>
+</tr>
+
+<!-- Donation details -->
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
+            <tr>
+                <td>Received with thanks from <b>${name}</b></td>
+            </tr>
+            <tr>
+                <td>Sum of Rupees(INR): <b>${amount}</b></td>
+            </tr>
+            <tr>
+                <td>Donation towards: <b>${project}</b></td>
+            </tr>
+            <tr>
+                <td>Address: <b>${address}</b></td>
+            </tr>
+        </table>
+    </td>
+</tr>
+
+<!-- Contact details -->
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
+            <tr>
+                <td>Contact: <b>+${phone}</b></td>
+            </tr>
+            <tr>
+                <td>PAN: <b>${PAN}</b></td>
+            </tr>
+            <tr>
+                <td>Email: <b>${email}</b></td>
+            </tr>
+        </table>
+    </td>
+</tr>
+
+<!-- Thank you message -->
+<tr>
+    <td colspan="2" style="border-bottom: 2px solid black;">
+        <div style="padding: 1rem 0;">
+            Thank You - Received <b>INR${amount}</b> for CARNATIC FOUNDATION
+        </div>
+    </td>
+</tr>
+
+<!-- Footer message -->
+<tr>
+    <td colspan="2">
+        <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
+            <tr>
+                <td
+                    style="/* font-family: monospace; */font-size: 12px;letter-spacing: -0.2;text-align: center;">
+                    This is a computer-generated receipt and no signature required
                 </td>
             </tr>
-    
-            <!-- Other sections... -->
             <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <div style="padding: 0.5rem 0; text-align: center;"><b>Donations are exempted Under 80-G of the Income
-                            Tax Act 1961</b></div>
-                </td>
-            </tr>
-    
-            <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
-                        <tr>
-                            <td><b>Form 12 A No:</b> AABTC8873EF2021101</td>
-                            <td><b>REG NO:</b> 56/2016</td>
-                        </tr>
-                        <tr>
-                            <td><b>80 G NUMBER:</b> AABTC8873EF20211</td>
-                            <td><b>PAN NO:</b> AABTC8873E</td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-    
-            <!-- Donation details -->
-            <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
-                        <tr>
-                            <td>Received with thanks from <b>${name}</b></td>
-                        </tr>
-                        <tr>
-                            <td>Sum of Rupees: <b>${amount}</b></td>
-                        </tr>
-                        <tr>
-                            <td>Donation towards: <b>${project}</b></td>
-                        </tr>
-                        <tr>
-                            <td>Address: <b>${address}</b></td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-    
-            <!-- Contact details -->
-            <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
-                        <tr>
-                            <td>Contact: <b>${phone}</b></td>
-                        </tr>
-                        <tr>
-                            <td>PAN: <b>${PAN}</b></td>
-                        </tr>
-                        <tr>
-                            <td>Email: <b>${email}</b></td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-    
-            <!-- Thank you message -->
-            <tr>
-                <td colspan="2" style="border-bottom: 2px solid black;">
-                    <div style="padding: 1rem 0;">
-                        Thank You - Received INR. <b>${amount}</b> For CARNATIC FOUNDATION
+                <td>
+                    <div style="margin-top: 1.2rem;">
+                        <div>For Queries</div>
+                        <div
+                            style="display: flex; flex-direction: row; justify-content: space-between; margin-top: 12px;">
+                            <div>Email: <b>trustee@carnaticfoundation.in</b></div>
+                            <div>Phone: <b>9884232121</b></div>
+                        </div>
                     </div>
                 </td>
             </tr>
-    
-            <!-- Footer message -->
-            <tr>
-                <td colspan="2">
-                    <table width="100%" cellpadding="0" cellspacing="0" style="padding: 1rem 0;">
-                        <tr>
-                            <td style="font-family: monospace; font-size: 0.875rem;">
-                                This is a computer-generated receipt and no signature required
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div style="display: flex; justify-content: space-between; margin-top: 0.5rem;">
-                                    <div>For Queries - Email: <b>trustee@carnaticfoundation.in</b></div>
-                                    <div>Phone: <b>9884232121</b></div>
-                                </div>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-    
         </table>
-    
+    </td>
+</tr>
+
+</table>
     
     </body>
     
--- carnatic-frontend/src/components/Footer.jsx
+++ carnatic-frontend/src/components/Footer.jsx
@@ -6,7 +6,8 @@ export default function Footer() {
         <div>
 
             <footer class=" w-full border-t-[1px] py-5 ">
-                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <div class="w-full flex flex-col md:flex-row space-y-4 md:space-y-0 items-center justify-between mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+
                     <span class="text-sm text-gray-500 sm:text-center  :text-gray-400">© 2023 <a href="#" class="hover:underline">Carnatic Foundation</a>. All Rights Reserved.
                     </span>
                     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  :text-gray-400 sm:mt-0">
@@ -19,6 +20,7 @@ export default function Footer() {
                         </li>
 
                     </ul>
+
                 </div>
             </footer>
 
