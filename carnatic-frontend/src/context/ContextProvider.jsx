b3a0fa94d31980a4a74ebfd5bff09b02d862b1c0
Hamjaster <hamzasepal@gmail.com>
2024-01-02 23:12:59 +0500

    updates.

--- carnatic-frontend/src/context/ContextProvider.jsx
+++ carnatic-frontend/src/context/ContextProvider.jsx
@@ -9,8 +9,8 @@ const MyContextProvider = ({ children }) => {
     const [donationInfo, setDonationInfo] = useState(
         {}
     )
-    const proxy = "http://localhost:5000"
-    // const proxy = "https://carnatic-backend.vercel.app"
+    // const proxy = "http://localhost:5000"
+    const proxy = "https://carnatic-backend.vercel.app"
 
     return (
         <MyContext.Provider value={{ proxy, donationInfo, setDonationInfo, member, setMember, project, setProject, userInfo, setUserInfo }}>
