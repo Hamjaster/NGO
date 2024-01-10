7536a616d668ace6572ae4e60cd5625e15b85daf
Hamjaster <hamzasepal@gmail.com>
2024-01-10 17:48:19 +0500

    hy

--- carnatic-frontend/src/dashboard/Transactions.jsx
+++ carnatic-frontend/src/dashboard/Transactions.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import DataTable from 'react-data-table-component';
 import { useParams, useSearchParams } from 'react-router-dom'
 import MyContext from '../context/context';
+import { useReactToPrint } from 'react-to-print';
 
 const tableCustomStyles = {
     headCells: {
@@ -44,6 +45,7 @@ export default function Transactions() {
     let name = queryParameters.get("name")
     const [data, setData] = useState([])
     const { proxy } = useContext(MyContext)
+    const pdfRef = useRef()
 
     const columns = [
         {
@@ -88,26 +90,34 @@ export default function Transactions() {
         getTransactions()
     }, [])
 
+    const donwloadPDf2 = useReactToPrint({
+        content: () => pdfRef.current,
+        documentTitle: 'Database',
+        onAfterPrint: () => console.log('Save pdf')
+    })
 
 
     return (
         <div>
             <div className="flex flex-col justify-evenly h-screen items-center">
-                <div className="text my-4 text-center text-5xl font-medium">
-                    Transaction Details of {name}
-                </div>
+                <div ref={pdfRef} className="transactions flex flex-col justify-evenly items-center space-y-6">
 
-                <div className="table">
-                    <DataTable
-                        columns={columns}
-                        data={data}
-                        customStyles={tableCustomStyles}
-                        pagination
-                    />
+                    <div className="text my-4 text-center text-5xl font-medium">
+                        Transaction Details of {name}
+                    </div>
+
+                    <div className="table">
+                        <DataTable
+                            columns={columns}
+                            data={data}
+                            customStyles={tableCustomStyles}
+                            pagination
+                        />
+                    </div>
                 </div>
 
                 <div className="buttons  w-full items-end justify-end flex flex-row space-x-5">
-                    <div className="bg-[#b5c3ff] hover:bg-[#92a6ff] cursor-pointer text-black rounded-xl px-10 py-3">
+                    <div onClick={donwloadPDf2} className="bg-[#b5c3ff] hover:bg-[#92a6ff] cursor-pointer text-black rounded-xl px-10 py-3">
                         Print / Download Statement
                     </div>
 
